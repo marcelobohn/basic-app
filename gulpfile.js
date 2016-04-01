a1ffc4663f0bdac747b201d5f0012cd657bb15af
Marcelo Bohn <marcelo.bohn@digitaldesk.com.br>
2016-04-01 10:17:45 -0300

    compile scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var clean = require('gulp-clean');
  
 gulp.task('webserver1', function() {
   gulp.src('app')
@@ -22,4 +24,29 @@ gulp.task('webserver3', function() {
   connect.server();
 });
 
-gulp.task('default', ['webserver3']);
+//cleaner dist
+gulp.task('clean', function(){
+  gulp.src(['dist/*'], {read:false})
+    .pipe(clean());
+});
+
+//compile sass
+gulp.task('sass', function () {
+  gulp.src(['app/scss/*.scss'])
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+//copy html
+gulp.task('html', function () {
+  gulp.src(['app/*.html'])
+    .pipe(gulp.dest('dist/'));
+});
+
+// Default task
+// gulp.task('default', ['webserver3']);
+gulp.task('default', function () {
+  gulp.start('clean');
+  gulp.start('html');
+  gulp.start('sass');
+});
